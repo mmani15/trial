b0fe477d36d96d49d821ae8b0c87621ae5d8824b
Kumari <mkumari@dxc.com>
2020-01-14 22:27:11 +0530

    html_tags

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-function test() {
-	console.log('Manisha,s first functiom');
-}
+// function test() {
+// 	console.log('Manisha,s first functiom');
+// }
 
-function second() {
-	console.log('second function');
-}
+// function second() {
+// 	console.log('second function');
+// }
 
-console.log(document.getElementById('manisha'))
-            document.getElementById('manisha').addEventListener('click', function() {
-                console.log('button clicked');
-            })
+// test();
+// second()
+// console.log(document.getElementById('manisha'))
+//             document.getElementById('manisha').addEventListener('click', function() {
+//                 console.log('button clicked');
+//             })
+
+
+function sum (a, b) {
+    return a + b;
+}
+console.log(process.argv);
+let ash = parseInt(process.argv[2], 10);
+let man = parseInt(process.argv[3], 10);
+console.log(sum(ash,man));
