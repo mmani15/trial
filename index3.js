36ddbf8cab00aa4cd730b1e4b567e66da52f4699
Kumari <mkumari@dxc.com>
2020-01-20 00:25:22 +0530

    new_p

--- index3.js
+++ index3.js
@@ -1,9 +1,6 @@
-<script>  
-var value=50;//global variable  
-function a(){  
-alert(value);  
-}  
-function b(){  
-alert(value);  
-}  
-</script>  
+function greet(firstName, lastName, honorific, greeting) {
+    return `${greeting} ${honorific} ${lastName}! I’m extremely pleased you could join us, ${firstName}! I hope you enjoy your stay, ${honorific} ${lastName}.`;
+  }
+  
+  console.log(greet("Brian", "Holt", "Lord", "Salutations"));
+  console.log(greet("Jack", "Sparrow", "Captain", "A-hoy"));
